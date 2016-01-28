621caf5695a17d712be63e6ec855ff93fdfa60aa
RanjitaG <ranjita.naik@careerbuilder.com>
2016-01-28 18:37:21 +0530

    job_details

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,8 @@ class JobsController < ApplicationController
   def index
     @jobs=Job.all
   end
+
+  def job_detail
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root to: 'jobs#index'
   devise_for :users
   resources :users
+
+  resources :jobs do
+    get "job_detail"
+  end
 end
